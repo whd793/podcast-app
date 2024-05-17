e1ad99bdd33e76b825471c7f4179d89f52c518b0
Won Lee <whd793@gmail.com>
2024-05-17 13:51:47 +0900

    added user routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
 
 //routes
-import authRoutes from './routes/auth.js';
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 dotenv.config();
@@ -13,26 +13,28 @@ dotenv.config();
 /** Middlewares */
 app.use(express.json());
 const corsConfig = {
-    credentials: true,
-    origin: true,
+  credentials: true,
+  origin: true,
 };
 app.use(cors(corsConfig));
-app.use(morgan('tiny'));
-app.disable('x-powered-by');
+app.use(morgan("tiny"));
+app.disable("x-powered-by");
 
 const port = process.env.PORT || 8700;
 
 const connect = () => {
-    mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.MONGO_URL).then(() => {
-        console.log('MongoDB connected');
-    }).catch((err) => {
-        console.log(err);
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
 
-
-app.use(express.json())
+app.use(express.json());
 // app.enable('trust proxy'); // optional, not needed for secure cookies
 // app.use(express.session({
 //     secret : '123456',
@@ -44,22 +46,22 @@ app.use(express.json())
 //     }
 // }));
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes)
 // app.use("/api/project", projectRoutes)
 // app.use("/api/team", teamRoutes)
 
-app.use((err, req, res, next)=>{
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong";        
-    return res.status(status).json({
-        success: false,
-        status,
-        message
-    })
-})
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
-app.listen(port,()=>{
-    console.log("Connected")
-    connect();
-})
+app.listen(port, () => {
+  console.log("Connected");
+  connect();
+});

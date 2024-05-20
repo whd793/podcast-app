978b774351ec745b47ae4caeb462761b99a7814d
Won Lee <whd793@gmail.com>
2024-05-20 16:30:47 +0900

    made some changes

--- client/src/utils/Themes.js
+++ client/src/utils/Themes.js
@@ -1,17 +1,17 @@
 export const darkTheme = {
     bg:"#15171E",
     bgLight: "#1C1E27",
-    primary:"#be1adb",
+    primary:"#f55874",
     text_primary:"#F2F3F4",
     text_secondary:"#b1b2b3",
-    card:"#121212",
+    card:"#1c1c1c",
     button:"#5c5b5b",
 }
 
 export const lightTheme = {
     bg:"#FFFFFF",
     bgLight: "#f0f0f0",
-    primary:"#be1adb",
+    primary:"#f55874",
     text_primary:"#111111",
     text_secondary:"#48494a",
     card:"#FFFFFF",

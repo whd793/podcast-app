5f0c1f5e0f072de256bfdca5b083eb2402ae5946
Won Lee <whd793@gmail.com>
2024-05-23 13:30:47 +0900

    modified server controllers & modified upload component

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cors from "cors";
-import morgan from "morgan";
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
 
 //routes
-import authRoutes from "./routes/auth.js";
-import podcastsRoutes from "./routes/podcast.js";
-import userRoutes from "./routes/user.js";
+import authRoutes from './routes/auth.js';
+import podcastsRoutes from './routes/podcast.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
 dotenv.config();
@@ -15,28 +15,26 @@ dotenv.config();
 /** Middlewares */
 app.use(express.json());
 const corsConfig = {
-  credentials: true,
-  origin: true,
+    credentials: true,
+    origin: true,
 };
 app.use(cors(corsConfig));
-app.use(morgan("tiny"));
-app.disable("x-powered-by");
+app.use(morgan('tiny'));
+app.disable('x-powered-by');
 
 const port = process.env.PORT || 8700;
 
 const connect = () => {
-  mongoose.set("strictQuery", true);
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-      console.log("MongoDB connected");
-    })
-    .catch((err) => {
-      console.log(err);
+    mongoose.set('strictQuery', true);
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+        console.log('MongoDB connected');
+    }).catch((err) => {
+        console.log(err);
     });
 };
 
-app.use(express.json());
+
+app.use(express.json())
 // app.enable('trust proxy'); // optional, not needed for secure cookies
 // app.use(express.session({
 //     secret : '123456',
@@ -48,23 +46,23 @@ app.use(express.json());
 //     }
 // }));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/podcasts", podcastsRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes)
+app.use("/api/podcasts", podcastsRoutes)
+app.use("/api/user", userRoutes)
 // app.use("/api/project", projectRoutes)
 // app.use("/api/team", teamRoutes)
 
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong";
-  return res.status(status).json({
-    success: false,
-    status,
-    message,
-  });
-});
+app.use((err, req, res, next)=>{
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong";        
+    return res.status(status).json({
+        success: false,
+        status,
+        message
+    })
+})
 
-app.listen(port, () => {
-  console.log("Connected");
-  connect();
-});
+app.listen(port,()=>{
+    console.log("Connected")
+    connect();
+})

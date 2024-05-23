a6af4899e1b106c3e2bf84fe7f133bc79d07ae9f
Won Lee <whd793@gmail.com>
2024-05-23 13:10:47 +0900

    made some changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,15 @@ import Dashboard from '../src/pages/Dashboard.jsx'
 import ToastMessage from './components/ToastMessage.jsx';
 import Search from '../src/pages/Search.jsx';
 import Favourites from '../src/pages/Favourites.jsx';
+import Profile from '../src/pages/Profile.jsx';
+import Podcasts from '../src/pages/Podcasts.jsx'
 import Upload from '../src/components/Upload.jsx';
+import DisplayPodcasts from '../src/pages/DisplayPodcasts.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useSelector } from "react-redux";
 import styled from 'styled-components';
+import AudioPlayer from "./components/AudioPlayer.jsx";
+import VideoPlayer from "./components/VideoPlayer.jsx";
 
 const Frame = styled.div`
   display: flex;
@@ -26,9 +31,9 @@ const Podstream = styled.div`
   flex-direction: row;
   width: 100%;
   height: 100vh;
+  background: ${({ theme }) => theme.bgLight};
   overflow-y: hidden;
   overflow-x: hidden;
-  background: ${({ theme }) => theme.bgLight}
 `;
 
 function App() {
@@ -38,6 +43,7 @@ function App() {
   const [SignUpOpen, setSignUpOpen] = useState(false);
   const [SignInOpen, setSignInOpen] = useState(false);
   const [uploadOpen,setUploadOpen] = useState(false);
+  const [videoOpen,setVideoOpen] = useState(false);
 
 
   const { currentUser } = useSelector(state => state.user);
@@ -50,16 +56,22 @@ function App() {
         {SignInOpen && <Signin setSignInOpen={setSignInOpen} setSignUpOpen={setSignUpOpen} />}
         {SignUpOpen && <Signup setSignInOpen={setSignInOpen} setSignUpOpen={setSignUpOpen} />}
         {uploadOpen && <Upload setUploadOpen={setUploadOpen} />}
+        {videoOpen && <VideoPlayer setVideoOpen={setVideoOpen} />}
         <Podstream>
-          <Menu darkMode={darkMode} setDarkMode={setDarkMode} setUploadOpen={setUploadOpen} />
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} setUploadOpen={setUploadOpen} setSignInOpen={setSignInOpen}/>
           <Frame>
             <Navbar setSignInOpen={setSignInOpen} setSignUpOpen={setSignUpOpen} />
             <Routes>
               <Route path='/' exact element={<Dashboard />} />
               <Route path='/search' exact element={<Search />} />
               <Route path='/favourites' exact element={<Favourites />} />
+              <Route path='/profile' exact element={<Profile />} />
+              <Route path='/podcasts/:id' exact element={<Podcasts />} />
+              <Route path='/showpodcasts/:type' exact element={<DisplayPodcasts />} />
+
             </Routes>
           </Frame>
+          <AudioPlayer/>
 
           {open && <ToastMessage open={open} message={message} severity={severity} />}
         </Podstream>

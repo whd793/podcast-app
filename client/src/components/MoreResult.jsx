da98abb21977b636aa772fb74f8715b786b3585d
Won Lee <whd793@gmail.com>
2024-05-26 14:40:47 +0900

    changed profile page style

--- client/src/components/MoreResult.jsx
+++ client/src/components/MoreResult.jsx
@@ -1,51 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Results = styled.div`
-background-color: ${({ theme }) => theme.bgLight};
-width: 700px;
-display: flex;
-align-items: center;
-padding: 8px;
-border-radius: 6px;
-gap: 20px;
-&:hover{
+const Results = styled(Link)`
+  background-color: ${({ theme }) => theme.bgLight};
+  width: 700px;
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  border-radius: 6px;
+  gap: 20px;
+  &:hover {
     cursor: pointer;
     transform: translateY(-8px);
     transition: all 0.4s ease-in-out;
     box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.3);
     filter: brightness(1.3);
   }
-`
+`;
 const PodcastImage = styled.img`
-height: 70px;
-border-radius: 4px;
-`
+  height: 70px;
+  border-radius: 4px;
+`;
 const PodcastInfo = styled.div`
-display: flex;
-flex-direction: column;
-gap: 8px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
 const PodcastName = styled.div`
-display: flex;
-flex-direction: column;
-color: ${({ theme }) => theme.text_primary};
-`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text_primary};
+`;
 const Creator = styled.div`
-color: ${({ theme }) => theme.text_secondary};
-font-size: 12px;
-
-`
-const MoreResult = ({podcast}) => {
+  color: ${({ theme }) => theme.text_secondary};
+  font-size: 12px;
+`;
+const MoreResult = ({ podcast }) => {
   return (
-    <Results>
-        <PodcastImage src={podcast?.thumbnail}/>
-        <PodcastInfo>
-            <PodcastName>{podcast?.name}</PodcastName>
-            <Creator>{podcast?.creator.name}</Creator>
-        </PodcastInfo>
+    <Results to={`/podcast/${podcast?._id}`}>
+      <PodcastImage src={podcast?.thumbnail} />
+      <PodcastInfo>
+        <PodcastName>{podcast?.name}</PodcastName>
+        <Creator>{podcast?.creator.name}</Creator>
+      </PodcastInfo>
     </Results>
-  )
-}
+  );
+};
 
-export default MoreResult
+export default MoreResult;

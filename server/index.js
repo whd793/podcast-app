d165d61d1f5d3dc736e8968dbb580b4822608f5d
Won Lee <whd793@gmail.com>
2024-06-02 15:50:47 +0900

    fixed server index.js

--- server/index.js
+++ server/index.js
@@ -15,26 +15,33 @@ dotenv.config();
 /** Middlewares */
 app.use(express.json());
 const corsConfig = {
-    credentials: true,
-    origin: true,
+  credentials: true,
+  origin: true,
 };
 app.use(cors(corsConfig));
-app.use(morgan('tiny'));
-app.disable('x-powered-by');
+// app.use(morgan('tiny'));
+// app.disable('x-powered-by');
+// app.use(function (request, response, next) {
+//     response.header("Access-Control-Allow-Origin", "*");
+//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 const port = process.env.PORT || 8700;
 
 const connect = () => {
-    mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.MONGO_URL).then(() => {
-        console.log('MongoDB connected');
-    }).catch((err) => {
-        console.log(err);
+  mongoose.set('strictQuery', true);
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log('MongoDB connected');
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
 
-
-app.use(express.json())
+app.use(express.json());
 // app.enable('trust proxy'); // optional, not needed for secure cookies
 // app.use(express.session({
 //     secret : '123456',
@@ -46,23 +53,23 @@ app.use(express.json())
 //     }
 // }));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/podcasts", podcastsRoutes)
-app.use("/api/user", userRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/podcasts', podcastsRoutes);
+app.use('/api/user', userRoutes);
 // app.use("/api/project", projectRoutes)
 // app.use("/api/team", teamRoutes)
 
-app.use((err, req, res, next)=>{
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong";        
-    return res.status(status).json({
-        success: false,
-        status,
-        message
-    })
-})
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || 'Something went wrong';
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
-app.listen(port,()=>{
-    console.log("Connected")
-    connect();
-})
+app.listen(port, () => {
+  console.log('Connected');
+  connect();
+});
